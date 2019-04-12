96ef14b7b9a6c7f3537c0cf8a4c7ec56c50fa3d8
imediasun <imediasun@gmail.com>
2019-04-12 14:51:53 +0300

    first commit

---
+++ src/Widget.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Imediasun\Widgets;
+
+class Widget
+{
+
+    protected $widgets; //массив доступных виджетов config/widgets.php
+
+
+    public function __construct()
+    {
+        $this->widgets = config('widgets');
+    }
+
+
+    public function show($obj, $data = [])
+    {
+
+        //Есть ли такой виджет
+        if (isset($this->widgets[$obj])) {
+
+            //создаем его объект передавая параметры в конструктор
+            $obj = new $this->widgets[$obj]($data);
+
+            //возвращаем результат выполнения
+            return $obj->execute();
+        }
+
+    }
+}
